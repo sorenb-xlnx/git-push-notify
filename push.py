15890febaf13a8ca42b484a644648a99e0cefd7c
Soren Brinkmann <soren.brinkmann@xilinx.com>
2016-02-07 12:10:04 -0800

    Add license header
    Release script under the Beerware license.
    
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>

--- push.py
+++ push.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+# "THE BEER-WARE LICENSE" (Revision 42):
+# Sören Brinkmann <soren.brinkmann@xilinx.com> wrote this file. As long as you
+# retain this notice you can do whatever you want with this stuff. If we meet
+# some day, and you think this stuff is worth it, you can buy me a beer in
+# return Sören Brinkmann
+
 import argparse
 import configparser
 import os
